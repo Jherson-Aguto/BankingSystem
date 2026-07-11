ee3922e192e688d0e17a8699d80a576bc88dd3a9
Jherson-Aguto <dalisayfernando4@gmail.com>
2026-07-11 16:27:51 +0800

    fix mapping issues

--- src/csbank.Application/Mapper/Customer.Mapper.cs
+++ src/csbank.Application/Mapper/Customer.Mapper.cs
@@ -9,15 +9,36 @@ internal static class Map
         new(dto.Id, dto.FirstName, dto.LastName,
         dto.Suffix, dto.RegistrationDate, dto.MiddleInitial);
 
-    public static CustomerDto ToDto(this CustomerRequest dto) =>
-        new(dto.Id, dto.FirstName, dto.LastName,
-        dto.Suffix, dto.RegistrationDate, dto.MiddleInitial);
+    public static CustomerDto ToDto(this CustomerRequest dto)
+    {
+        return new CustomerDto
+        {
+            Id = dto.Id,
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            Suffix = dto.Suffix,
+            RegistrationDate = dto.RegistrationDate,
+            MiddleInitial = dto.MiddleInitial
+        };
+    }
 
     public static PrivateInfoRequest ToDomain(this PrivateInfoDto dto) =>
         new(dto.Id, dto.Email, dto.PhoneNumber, dto.City, dto.Province,
         dto.Country, dto.Nationality, dto.Age, dto.BirthDate);
 
-    public static PrivateInfoDto ToDto(this PrivateInfoRequest dto) =>
-        new(dto.Id, dto.Email, dto.PhoneNumber, dto.City, dto.Province,
-        dto.Country, dto.Nationality, dto.Age, dto.BirthDate);
+    public static PrivateInfoDto ToDto(this PrivateInfoRequest dto)
+    {
+        return new PrivateInfoDto
+        {
+            Id = dto.Id,
+            Email = dto.Email,
+            PhoneNumber = dto.PhoneNumber,
+            City = dto.City,
+            Province = dto.Province,
+            Country = dto.Country,
+            Nationality = dto.Nationality,
+            Age = dto.Age,
+            BirthDate = dto.BirthDate
+        };
+    }
 }
