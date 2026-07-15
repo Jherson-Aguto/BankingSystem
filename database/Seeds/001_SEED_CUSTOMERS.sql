14d6e546a159edd3139e9dc25bf9ccb91f87d831
Jherson-Aguto <dalisayfernando4@gmail.com>
2026-07-15 19:16:47 +0800

    Seeder for customer details and its accounts

--- database/Seeds/001_SEED_CUSTOMERS.sql
+++ database/Seeds/001_SEED_CUSTOMERS.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-WITH id AS(
+WITH customer AS(
     INSERT INTO users.customer_details(
         first_name,
         last_name,
@@ -16,7 +16,8 @@ WITH id AS(
         'M'
     )
     RETURNING id
-)
+),
+private_info AS (
 INSERT INTO users.private_information(
     customer_id,
     email,
@@ -28,16 +29,67 @@ INSERT INTO users.private_information(
     birth_date
 )
 SELECT
-    id,
+    customer.id,
     'agutojherson@gmail.com',
-    '+63 968 645 0568',
+    '+639686450568',
     'Echague',
     'Isabela',
     'Philippines',
     'Filipino',
-    '2004-05-16'
-FROM id;
-
-
+    '2004-05-16'::DATE
+FROM customer
+),
+account AS (
+INSERT INTO accounts.account_details(
+    customer_id,
+    account_number,
+    currency,
+    created_at,
+    account_status
+)
+SELECT
+    customer.id,
+    'PH31BNOR00000012345678',
+    'PHP',
+    CURRENT_TIMESTAMP,
+    'Active'
+FROM customer
+RETURNING id
+),
+checking AS (
+INSERT INTO accounts.checking_account(
+    account_id,
+    balance,
+    overdraft_limit,
+    modes_of_payment,
+    interest_rate,
+    fees
+)
+SELECT
+    account.id,
+    105000,
+    0,
+    'Online',
+    6,
+    0
+FROM account
+),
+savings AS (
+INSERT INTO accounts.savings_account(
+    account_id,
+    balance,
+    withdrawal_usage,
+    interest_rate,
+    fees
+)
+SELECT
+    account.id,
+    253,
+    300,
+    6,
+    0
+FROM account
+)
+SELECT * FROM customer;
 
 ROLLBACK;
