09d11e2b7dfb00af92bb38609547abfb153fb0ed
Jherson-Aguto <dalisayfernando4@gmail.com>
2026-07-16 14:23:53 +0800

    Updated Contructor injection

--- src/csbank.Application/Services/RegisterCustomer.cs
+++ src/csbank.Application/Services/RegisterCustomer.cs
@@ -5,9 +5,8 @@ using CSBank.Application.Models;
 
 namespace CSBank.Application.Services;
 
-public class RegisterCustomerService : IRegisterCustomer
+public class RegisterCustomerService(RegisterServiceDomain register) : IRegisterCustomer
 {
-    private readonly RegisterServiceDomain register = new();
 
     public async Task<(CustomerDto customerData, PrivateInfoDto privateInfoData)>
     Register(CustomerDto customerDto, PrivateInfoDto privateInfo)
