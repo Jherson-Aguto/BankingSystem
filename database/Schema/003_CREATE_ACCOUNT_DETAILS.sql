80127f47d8aa7024aa1fef40025b3c012bde4556
Jherson-Aguto <dalisayfernando4@gmail.com>
2026-07-26 12:57:19 +0800

    redesigned account again

--- database/Schema/003_CREATE_ACCOUNT_DETAILS.sql
+++ database/Schema/003_CREATE_ACCOUNT_DETAILS.sql
@@ -2,11 +2,15 @@ BEGIN;
 
 CREATE TYPE accounts.account_status AS ENUM ('Active', 'Frozen', 'Closed');
 
+CREATE TYPE accounts.account_types AS ENUM ('Savings', 'Checking');
+
 CREATE TABLE IF NOT EXISTS accounts.account_details(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     customer_id UUID REFERENCES users.Customer_Details(id) ON DELETE CASCADE,
     account_number VARCHAR(100) NOT NULL UNIQUE,
+    account_type accounts.account_types,
     currency VARCHAR(100) NOT NULL,
+    balance NUMERIC(12, 4) NOT NULL CHECK(balance >= 0) DEFAULT 0,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     account_status accounts.account_status NOT NULL     
 );
@@ -15,7 +19,6 @@ CREATE TYPE accounts.modes_of_payment AS ENUM ('Debit', 'Check', 'Online');
 
 CREATE TABLE IF NOT EXISTS accounts.checking_account(
     account_id UUID PRIMARY KEY REFERENCES accounts.account_details(id) ON DELETE CASCADE,
-    balance NUMERIC(12, 4) NOT NULL CHECK(balance >= 0) DEFAULT 0,
     overdraft_limit NUMERIC(12, 4) NOT NULL CHECK(overdraft_limit >= 0) DEFAULT 0,
     modes_of_payment accounts.modes_of_payment NOT NULL DEFAULT 'Online',
     interest_rate NUMERIC(12, 4) CHECK(interest_rate BETWEEN 0 AND 100),
@@ -24,7 +27,6 @@ CREATE TABLE IF NOT EXISTS accounts.checking_account(
 
 CREATE TABLE IF NOT EXISTS accounts.savings_account(
     account_id UUID PRIMARY KEY REFERENCES accounts.account_details(id) ON DELETE CASCADE,
-    balance NUMERIC(12, 4) NOT NULL CHECK(balance >= 0) DEFAULT 0,
     withdrawal_usage NUMERIC(12, 4) NOT NULL CHECK (withdrawal_usage >= 0) DEFAULT 0,
     interest_rate NUMERIC CHECK(interest_rate BETWEEN 0 AND 100),
     fees NUMERIC(12, 4) CHECK(fees >= 0)
